378fdb881e410618489df4af7946b6820a1610c0
lhk3337 <lhk3337@naver.com>
2021-12-30 21:22:49 +0900

    room Notifications

--- src/public/app.js
+++ src/public/app.js
@@ -1,10 +1,12 @@
 const socket = io(); // io함수는 socket.io가 실행하고 있는 서버를 찾음
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
+const form = welcome.querySelector("form");
+
 room.hidden = true;
+let roomName;
 
 const addMessage = (message) => {
   const ul = room.querySelector("ul");
@@ -13,17 +15,30 @@ const addMessage = (message) => {
   ul.appendChild(li);
 };
 
-let roomName;
+const handleMessageSubmit = (event) => {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+};
+
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room :${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
+};
+
 const handleRoomSubmit = (event) => {
   event.preventDefault();
   const input = form.querySelector("input");
 
-  socket.emit("enter_room", input.value, () => {
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room :${roomName}`;
-  }); // room이라는 이벤트를 서버로 emit함, argument로 object가 될 수 있음
+  socket.emit("enter_room", input.value, showRoom); // room이라는 이벤트를 서버로 emit함, argument로 object가 될 수 있음
   roomName = input.value;
   input.value = "";
 };
@@ -31,4 +46,10 @@ const handleRoomSubmit = (event) => {
 socket.on("welcome", () => {
   addMessage("someone joined");
 });
+
+socket.on("bye", () => {
+  addMessage("someone left!!");
+});
+socket.on("new_message", addMessage);
+
 form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -39,6 +39,13 @@ wsServer.on("connection", (socket) => {
     //   done("hello from the backend");
     // }, 5000);
   });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
+  });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
